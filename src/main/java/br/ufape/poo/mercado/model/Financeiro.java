75f4a46868e62b32614fe86e8dd39b2eaa15a292
LuanTCarvalho <luan.teixeira.car@gmail.com>
2026-08-03 06:03:40 -0300

    Get e set de cliente, estoque, financeiro, fornecedor e funcionario

--- src/main/java/br/ufape/poo/mercado/model/Financeiro.java
+++ src/main/java/br/ufape/poo/mercado/model/Financeiro.java
@@ -17,4 +17,63 @@ public class Financeiro {
     private Double lucro;
     private String dataRegistro;
 
+    //construtor
+    public Financeiro(Double receita, Double despesa, Double lucro, String dataRegistro) {
+        this.receita = receita;
+        this.despesa = despesa;
+        this.lucro = lucro;
+        this.dataRegistro = dataRegistro;
+    }
+
+    //get e set
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Double getReceita() {
+        return receita;
+    }
+
+    public void setReceita(Double receita) {
+        this.receita = receita;
+    }
+
+    public Double getDespesa() {
+        return despesa;
+    }
+
+    public void setDespesa(Double despesa) {
+        this.despesa = despesa;
+    }
+
+    public Double getLucro() {
+        return lucro;
+    }
+
+    public void setLucro(Double lucro) {
+        this.lucro = lucro;
+    }
+
+    public String getDataRegistro() {
+        return dataRegistro;
+    }
+
+    public void setDataRegistro(String dataRegistro) {
+        this.dataRegistro = dataRegistro;
+    }
+
+    //função
+    
+    public Double calcularLucro() {
+        double rec = (this.receita != null) ? this.receita : 0.0;
+        double desp = (this.despesa != null) ? this.despesa : 0.0;
+        
+        this.lucro = rec - desp;
+        return this.lucro;
+    }
 }
