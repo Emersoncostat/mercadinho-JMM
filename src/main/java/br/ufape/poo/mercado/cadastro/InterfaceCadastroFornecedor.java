066ccdccf4b19cbc60e0e489dea059443fdfe70a
Bina-Git <so.bina.contact@gmail.com>
2026-08-11 16:21:26 -0300

    Interface para o service de Fornecedor

---
+++ src/main/java/br/ufape/poo/mercado/cadastro/InterfaceCadastroFornecedor.java
@@ -0,0 +1,19 @@
+package br.ufape.poo.mercado.cadastro;
+
+import java.util.List;
+
+import br.ufape.poo.mercado.model.Fornecedor;
+import br.ufape.poo.mercado.negocio.excecoes.EntidadeNaoEncontradaException;
+
+public interface  InterfaceCadastroFornecedor {
+
+    Fornecedor salvarFornecedor(Fornecedor entity);
+
+    Fornecedor procurarFornecedorId(Integer id) throws EntidadeNaoEncontradaException;
+
+    List<Fornecedor> listarFornecedores();
+
+    boolean verificarExistenciaFornecedorId(Integer id);
+
+    void removerFornecedorId(Integer id) throws EntidadeNaoEncontradaException;
+}
